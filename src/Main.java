b4ea783bd1723b1dac412745a0022d589a64bbe0
Антон Дмитриев <highrsalt@gmail.com>
2022-09-26 22:43:35 +0400

    Переработаны названия переменных

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticket_cost = 2549; // стоимость билета
+        int ticketCost = 2549; // стоимость билета
         int mile = 20; // "стоимость" одной мили
 
-        System.out.println("при покупке билета стоимостью " + ticket_cost + " вы получите " + (ticket_cost / mile) + " бонусных миль ");
+        System.out.println("При покупке билета стоимостью " + ticketCost + " вы получите " + (ticketCost / mile) + " бонусных миль ");
     }
 }
